2b1b65f5119b16019395cdcc3d344871555a791d
Zane-Clegg <zanerclegg@gmail.com>
2023-03-17 19:10:57 -0600

    Working

---
+++ Horse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HorseRacing
+{
+    public class Horse
+    {
+        public Random rnd = new Random();
+        int lottery;
+        int positionX;
+        int positionY;
+        string name;
+
+
+        public Horse(int posx, int posy, string name) {
+            lottery = 1;
+            positionX = posx;
+            positionY = posy;
+            this.name = name;
+        }
+        public bool Move()
+        {
+            if (lottery == rnd.Next(1, 1001))
+            {
+                Console.SetCursorPosition(positionX, positionY);
+                Console.Write("-");
+                positionX += 1;
+                if (positionX == 57)
+                {
+                    Console.SetCursorPosition(28, 4);
+                    Console.Write($"{name}");
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace HorseRacing
@@ -14,20 +15,62 @@ namespace HorseRacing
         //Todo: 5) The first horse to travel 50 spaces is pronounced the winner
         //Todo: 6) 2 - All kids die appropriately  (hint - how did we do this in Matrix??)
 
-
+        static SemaphoreSlim semaphore = new SemaphoreSlim(1,1);
+        static bool horseWon = false;
+        public static void HThread(Horse h)
+        {
+            while (true)
+            {
+                semaphore.Wait();
+                if (horseWon)
+                {
+                    semaphore.Release();
+                    return;
+                }
+                if (h.Move() == true)
+                {
+                    horseWon = true;
+                }
+                semaphore.Release();
+                if (horseWon)
+                {
+                    return;
+                }
+            }
+        }
         public static void DrawScreen()
         {
+            string[] names = new string[3] { "HorseA", "HorseB", "HorseC" };
+            Random rnd = new Random();
             Console.SetCursorPosition(0, 1);
             Console.WriteLine("HorseA:");
             Console.WriteLine("HorseB:");
             Console.WriteLine("HorseC:");
+            List<Thread> horses = new List<Thread>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Horse h = new Horse(7,i+1, names[i]);
+                horses.Add(new Thread(()=>HThread(h)));
+            }
+
+            foreach (Thread t in horses)
+            {
+                t.Start();
+            }
+            foreach (Thread t in horses)
+            {
+                t.Join();
+            }
         }
+
         static void Main(string[] args)
         {
             Console.SetCursorPosition(0, 0);
             Console.WriteLine("Welcome to the Racing Program:");
             DrawScreen();
-            Console.WriteLine("The winner of this race was");//who?
+            Console.SetCursorPosition(0, 4);
+            Console.WriteLine("The winner of this race was");
         }
     }
 }
